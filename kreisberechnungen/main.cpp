559bf6f2b513db30f637b52f4fc2abfd8e4eeb28
tfast <till@eduroam-056-041.scc.uni-weimar.de>
2015-03-05 17:34:19 +0100

    added functions area and circumference working

--- kreisberechnungen/main.cpp
+++ kreisberechnungen/main.cpp
@@ -1,7 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+
+float circumference(float r){
+	float circumference = 0;
+	circumference = 2 * M_PI * r;
+	return circumference;
+}
+
+float area(float r){
+	float area = 0;
+	area = M_PI * r * r;
+	return area;
+}
 
 
 int main(){
-
+	float radius = 0;
+	std::cout << "Bitte Radius eingeben: " << std::endl;
+	std::cin >> radius;
+	std::cout << "Umfang: " << circumference(radius) << std::endl;
+	std::cout << "Flächeninhalt: " << area(radius) << std::endl;
 
 	return 0;
 }
